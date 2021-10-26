3f8198a6e790b33513625e2c1047a677802ad33c
throne.yash@gmail.com <throne.yash@gmail.com>
2021-10-26 13:47:57 -0600

    Adding another problem

---
+++ Find_Remainder.js
@@ -0,0 +1,42 @@
+// Write a program to find the remainder when an integer A is divided by an integer B.
+
+// Input
+// The first line contains an integer T, the total number of test cases. Then T lines follow, each line contains two Integers A and B.
+
+// Output
+// For each test case, find the remainder when A is divided by B, and display it in a new line.
+
+// Constraints
+// 1 ≤ T ≤ 1000
+// 1 ≤ A,B ≤ 10000
+// Example
+// Input
+// 3
+// 1 2
+// 100 200
+// 40 15
+
+// Output
+// 1
+// 100
+// 10
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+
+let input = ''
+process.stdin.on('data', function (data) {
+  input = input + data
+})
+process.stdin.on('end', function () {
+  input = input.split('\n')
+  let k = Number(input[0])
+  let output = input
+    .slice(1, k + 1)
+    .map((x) => {
+      let [a, b] = x.split(' ').map(Number)
+      return a % b
+    })
+    .join('\n')
+  console.log(output)
+})
